6a08d4cb449e8d036ed6bad89fe8904e1f7ea29c
Amit <amiteliav1.spammer@gmail.com>
2016-06-19 00:45:58 +0300

    Fix was string.

--- theorms.py
+++ theorms.py
@@ -1,8 +1,8 @@
 from theorm import Theorm, Clause, Given
 import numbers
 
-# zero_axiom = Theorm('Zero Axiom', Given([Clause('a', '==', '0'), Clause('b', '.isalnum()')]),
-#                     'a * b == 0')
+zero_axiom = Theorm('Zero Axiom', Given([Clause('a', '==', 0), Clause('b', 'isinstance()', 'numbers.Number')]),
+                    'a * b == 0')
 
 commutative_addition = Theorm('Addition Commutativity', Given([Clause('a', 'isinstance()', 'numbers.Number'),
                                                                Clause('b', 'isinstance()', 'numbers.Number')]),
